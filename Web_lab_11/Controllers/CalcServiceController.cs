927daadd8e8a2bfe6e17a710da3c2495a18f2962
elik <elik@example.com>
2024-11-30 22:04:50 +0700

    first commit

---
+++ Web_lab_11/Controllers/CalcServiceController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_lab_11.Models;
+
+namespace Web_lab_11.Controllers
+{
+    public class CalcServiceController : Controller
+    {
+        private Random rand = new Random();
+
+        [ViewData]
+        public required CalcServiceModel injectedCalc { get; set; }
+
+        public IActionResult PassUsingModel()
+        {
+            int randValueFirst = rand.Next(1, 10);
+            int randValueSecond = rand.Next(1, 10);
+            var viewModel = new CalcServiceModel()
+            {
+                RandValueFirst = randValueFirst.ToString(),
+                RandValueSecond = randValueSecond.ToString(),
+                Add = randValueFirst.ToString() + " + " + randValueSecond.ToString() + " = " + (randValueFirst + randValueSecond).ToString(),
+                Sub = randValueFirst.ToString() + " - " + randValueSecond.ToString() + " = " + (randValueFirst - randValueSecond).ToString(),
+                Mult = randValueFirst.ToString() + " * " + randValueSecond.ToString() + " = " + (randValueFirst * randValueSecond).ToString(),
+                Div = randValueSecond == 0 ? "Ошибка второй параметр 0. Деление на 0." : randValueFirst.ToString() + " / " + randValueSecond.ToString() + " = " + (randValueFirst / randValueSecond).ToString()
+
+            };
+            return View(viewModel);
+        }
+
+        public IActionResult PassUsingViewData()
+        {
+                int randValueFirst = rand.Next(0, 10);
+                int randValueSecond = rand.Next(0, 10);
+                ViewData["Calc"] = new CalcServiceModel()
+                {
+                    RandValueFirst = randValueFirst.ToString(),
+                    RandValueSecond = randValueSecond.ToString(),
+                    Add = randValueFirst.ToString() + " + " + randValueSecond.ToString() + " = " + (randValueFirst + randValueSecond).ToString(),
+                    Sub = randValueFirst.ToString() + " - " + randValueSecond.ToString() + " = " + (randValueFirst - randValueSecond).ToString(),
+                    Mult = randValueFirst.ToString() + " * " + randValueSecond.ToString() + " = " + (randValueFirst * randValueSecond).ToString(),
+                    
+                    Div = randValueSecond == 0 ? "Ошибка второй параметр 0. Деление на 0.":randValueFirst.ToString() + " / " + randValueSecond.ToString() + " = " + (randValueFirst / randValueSecond).ToString()
+
+                };
+                return View();
+        }
+        public IActionResult PassUsingViewBag() {
+            int randValueFirst = rand.Next(0, 10);
+            int randValueSecond = rand.Next(0, 10);
+            ViewBag.Calc = new CalcServiceModel()
+            {
+                RandValueFirst = randValueFirst.ToString(),
+                RandValueSecond = randValueSecond.ToString(),
+                Add = randValueFirst.ToString() + " + " + randValueSecond.ToString() + " = " + (randValueFirst + randValueSecond).ToString(),
+                Sub = randValueFirst.ToString() + " - " + randValueSecond.ToString() + " = " + (randValueFirst - randValueSecond).ToString(),
+                Mult = randValueFirst.ToString() + " * " + randValueSecond.ToString() + " = " + (randValueFirst * randValueSecond).ToString(),
+                Div = randValueSecond == 0 ? "Ошибка второй параметр 0. Деление на 0." : randValueFirst.ToString() + " / " + randValueSecond.ToString() + " = " + (randValueFirst / randValueSecond).ToString()
+
+            };
+            return View();
+        }
+
+
+        public IActionResult AccessServiceDirectly()
+        {
+            int randValueFirst = rand.Next(0, 10);
+            int randValueSecond = rand.Next(0, 10);
+            injectedCalc = new CalcServiceModel()
+            {
+                RandValueFirst = randValueFirst.ToString(),
+                RandValueSecond = randValueSecond.ToString(),
+                Add = randValueFirst.ToString() + " + " + randValueSecond.ToString() + " = " + (randValueFirst + randValueSecond).ToString(),
+                Sub = randValueFirst.ToString() + " - " + randValueSecond.ToString() + " = " + (randValueFirst - randValueSecond).ToString(),
+                Mult = randValueFirst.ToString() + " * " + randValueSecond.ToString() + " = " + (randValueFirst * randValueSecond).ToString(),
+                Div = randValueSecond == 0 ? "Ошибка второй параметр 0. Деление на 0." : randValueFirst.ToString() + " / " + randValueSecond.ToString() + " = " + (randValueFirst / randValueSecond).ToString()
+
+            };
+            return View();
+        }
+    }
+}
---
+++ Web_lab_11/Models/CalcModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Web_lab_11.Models
+{
+    public class CalcServiceModel
+    {
+        public required string RandValueFirst { get; set; }
+        public required string RandValueSecond { get; set; }
+        public required string Add { get; set; }
+        public required string Sub { get; set; }
+        public required string Mult { get; set; }
+        public required string Div { get; set; }
+    }
+}
+
